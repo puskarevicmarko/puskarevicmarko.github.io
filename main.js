6d90f722f69d75bb5af84f54af5c2cb22c3f2413
puskarevicmarko <puskarevic.marko@gmail.com>
2020-04-15 20:32:27 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const CHART = document.getElementById("lineChart");
 console.log(CHART);
 
 setTimeout(function(){
-    odometer.innerHTML = 150;
+    odometer.innerHTML = 151;
 }, 0);
 
 window.odometerOptions = {
@@ -13,7 +13,7 @@ Chart.defaults.global.defaultFontFamily = "'-apple-system', 'BlinkMacSystemFont'
 let lineChart = new Chart(CHART, {
     type: 'line',
     data: {
-        labels: ["16-Mar", "17-Mar", "18-Mar", "19-Mar", "20-Mar", "21-Mar", "22-Mar", "23-Mar", "24-Mar", "25-Mar", "26-Mar", "27-Mar", "28-Mar", "29-Mar", "30-Mar", "31-Mar", "1-Apr", "2-Apr", "3-Apr", "4-Apr", "5-Apr", "6-Apr", "7-Apr", "8-Apr", "9-Apr", "10-Apr", "11-Apr", "12-Apr", "13-Apr", "14-Apr"],
+        labels: ["16-Mar", "17-Mar", "18-Mar", "19-Mar", "20-Mar", "21-Mar", "22-Mar", "23-Mar", "24-Mar", "25-Mar", "26-Mar", "27-Mar", "28-Mar", "29-Mar", "30-Mar", "31-Mar", "1-Apr", "2-Apr", "3-Apr", "4-Apr", "5-Apr", "6-Apr", "7-Apr", "8-Apr", "9-Apr", "10-Apr", "11-Apr", "12-Apr", "13-Apr", "14-Apr", "15-Apr"],
         datasets: [
             {
                 label: "Lexington COVID-19 Confirmed Cases",
@@ -30,7 +30,7 @@ let lineChart = new Chart(CHART, {
                 pointBorderWidth: 1,
                 pointHoverRadius: 5,
                 pointHitRadius: 10,
-                data: [3, null, null, null, null, null, 8, null, null, null, null, null, 25, 25, 25, null,null,33,36,null,null,null,47,62,null,null,null,130,139,150],
+                data: [3, null, null, null, null, null, 8, null, null, null, null, null, 25, 25, 25, null,null,33,36,null,null,null,47,62,null,null,null,130,139,150,151],
                 spanGaps: true,
             }
         ]
